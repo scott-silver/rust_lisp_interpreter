232b43ae995c29699b6ff429b8e291b8c9385fb1
Scott Silver <silversh@gmail.com>
2021-04-30 18:43:31 -0700

    add step arg

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::env;
 use std::io::{self, Write};
 
 mod step_0;
 mod step_1;
 
+fn rep_for_step(input: &String, step: u32) -> &str {
+    match step {
+        0 => step_0::rep(&input),
+        1 => step_1::rep(&input),
+        _ => step_1::rep(&input),
+    }
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let step_arg = args.get(1);
+    let step_arg_num = match step_arg {
+        Some(x) => x.parse::<u32>().unwrap(),
+        _ => 0,
+    };
+
     loop {
         print!("user> ");
 
@@ -18,6 +34,6 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        println!("{}", step_0::rep(&input));
+        println!("{}", rep_for_step(&input, step_arg_num));
     }
 }
